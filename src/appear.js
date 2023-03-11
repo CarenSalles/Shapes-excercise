d6557bd31561f50ca362bb6b372520a6f86fa5d3
Caren Salles <carencrisbsalles@gmail.com>
2023-03-12 00:36:30 +0100

    appear / disappear functions created

---
+++ src/appear.js
@@ -0,0 +1,5 @@
+function reset(){
+    circle.style.display = "block";
+    square.style.display = "block";
+    triangle.style.display = "block";
+}
---
+++ src/disappear.js
@@ -0,0 +1,30 @@
+let circle = document.getElementById('container-shapes__circle');
+let square = document.getElementById('container-shapes__square');
+let triangle = document.getElementById('container-shapes__triangle');
+
+function disappearCircle() {
+
+ if(circle.style.display = 'block'){
+
+  circle.style.display = 'none';
+ }
+
+}
+
+function disappearSquare() {
+
+  if (square.style.display = 'block') {
+    
+   square.style.display = 'none';
+ }
+    
+}
+
+function disappearTriangle() {
+
+  if (triangle.style.display = 'block') {
+        
+  triangle.style.display = 'none';        
+ }
+    
+}
